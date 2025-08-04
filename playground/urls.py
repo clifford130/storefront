624eabcdf2ff063946fff741372b83226ebddf63
clifford130 <cliffordonchomba483@gmail.com>
2025-08-04 14:39:19 +0300

    added templates and debugged url config

--- playground/urls.py
+++ playground/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 # urls config
 urlpatterns = [
-    path('', views.playground_home, name='playground_home'),
-    path('hello/', views.say_hello)
+    # path('', views.playground_home, name='playground_home'),
+    path('', views.say_hello)
 ]
--- playground/views.py
+++ playground/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
-    return  HttpResponse("Hello, World! This is the playground app.")
-
-def playground_home(request):
-    return HttpResponse("Welcome to the playground!")
+    # return  HttpResponse("Hello, World! This is the playground app.")
+    return render(request,'hello.html',{'name':'clifford'})

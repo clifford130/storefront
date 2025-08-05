2f0c92574db06e54cab3fe06cdaecf627bdd576d
clifford130 <cliffordonchomba483@gmail.com>
2025-08-05 11:00:04 +0300

    addded classes to models.py in store app and created likes app

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title=models.CharField(max_length=255)
-    featured_product=models.Foreignkey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    featured_product=models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):

55cf6b3e4ffa9c8916c277e786adf0e49e1dfecd
clifford130 <cliffordonchomba483@gmail.com>
2025-08-05 10:16:46 +0300

    finished creating classes to store app

--- store/models.py
+++ store/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
+class Promotion(models.Model):
+    description=models.CharField(max_length=255)
+    discount=models.FloatField()
+
 class Collection(models.Model):
     title=models.CharField(max_length=255)
+    featured_product=models.Foreignkey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
-class Product(models.model):
+class Product(models.Model):
     title=models.CharField(max_length=255)
-    description=models.models.TextField()
+    description=models.TextField()
     price=models.DecimalField(max_digits=6,decimal_places=2)
     inventory=models.IntegerField()
     last_update=models.DateTimeField(auto_now=True)
     Collection=models.ForeignKey('Collection',on_delete=models.PROTECT)
+    promotion=models.ManyToManyField(Promotion)
 
-class customer(models.model):
+class Customer(models.Model):
     MEMBERSHIP_BRONZE='B'
     MEMBERSHIP_SILVER='S'
     MEMBERSHIP_GOLD='G'
@@ -23,12 +29,12 @@ class customer(models.model):
     ]
     first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
-    email=models.models.EmailField(unique=True)
+    email=models.EmailField(unique=True)
     phone=models.CharField(max_length=255)
     birth_date=models.DateField(null=True)
-    membership=models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default='MEMBERSHIP_BRONZE')
+    membership=models.CharField(max_length=255, choices=MEMBERSHIP_CHOICES, default='MEMBERSHIP_BRONZE')
 
-class Order(models.model):
+class Order(models.Model):
     PAYMENT_STATUS_PENDING='P'
     PAYMENT_STATUS_COMPLETE='C'
     PAYMENT_STATUS_FAILED='F'
@@ -38,14 +44,30 @@ class Order(models.model):
         (PAYMENT_STATUS_FAILED,'Failed')    
     ]
     placed_at=models.DateTimeField(auto_now_add=True)
-    payment_status=models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    payment_status=models.CharField(max_length=255, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer=models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order, on_delete=models.PROTECT)
+    product=models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity=models.PositiveIntegerField() #prevents -ve values
+    unit_price=models.DecimalField(max_digits=6, decimal_places=2)
     
-class Address(models.model):
-    street=models.models.CharField(max_length=255)
-    city=models.models.CharField(max_length=255)
+class Address(models.Model):
+    street=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
     # customer=models.OneToOneField(customer,on_delete=models.CASCADE, primary_key=True)   #one to on relationship
     customer=models.ForeignKey(
-        customer,on_delete=models.CASCADE) 
+        Customer,on_delete=models.CASCADE) #on eto many relationship
+    
+class Cart(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveSmallIntegerField()
+    
 
 
     
